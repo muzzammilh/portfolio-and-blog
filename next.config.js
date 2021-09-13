97c8e4a2f61de865bdd09f73031c01c5354182da
your username <muzzammilz@gmail.com>
2021-09-13 22:17:59 +0500

    Adding more configurations and gray-matter

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+  target: 'serverless',
+  webpack: function (config) {
+    config.module.rules.push({test:  /\.md$/, use: 'raw-loader'});
+    config.module.rules.push({test: /\.yml$/, use: 'raw-loader'});
+    return config;
+  }
+};
