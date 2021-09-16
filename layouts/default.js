d059c700e8369d68739b385f9e65e071adca61f6
your username <muzzammilz@gmail.com>
2021-09-16 18:37:57 +0500

    Ignored DS_Store and added components dir

--- layouts/default.js
+++ layouts/default.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import Header from '@layouts/header';
-import Footer from '@layouts/footer';
+import Header from '@components/header';
+import Footer from '@components/footer';
 
 export default function DefaultLayout(props) {
   return (
