c80da00577d06b51402b8d25d10ecb1722ede85d
your username <muzzammilz@gmail.com>
2021-09-17 10:16:35 +0500

    Added foot note + custom Google font

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
     <footer>
-      <p>©2020 | Footer</p>
+      <div className="flex my-4 justify-center items-center flex-col text-lg">
+        <div className="my-4">
+          <a href="#" target="_blank" rel="noreferrer" className="px-3">LinkedIn</a>
+          <a href="#" target="_blank" rel="noreferrer" className="px-3">GitHub</a>
+          <a href="#" target="_blank" rel="noreferrer" className="px-3">Resume</a>
+        </div>
+        <p className="col-end-3">
+          Designed and Developed by<span className="bg-gray-200 p-2 ml-2 rounded-sm">Muzzammil Hussain</span>
+        </p>
+      </div>
     </footer>
   );
 }
--- layouts/default.js
+++ layouts/default.js
@@ -5,13 +5,15 @@ import Footer from '@components/footer';
 export default function DefaultLayout(props) {
   return (
     <main>
-      <Head>
-        <title>{props.title}</title>
-        <meta name="description" content={props.description} />
-      </Head>
-      <Header />
-      {props.children}
-      <Footer />
+      <div className="container mx-auto">
+        <Head>
+          <title>{props.title}</title>
+          <meta name="description" content={props.description} />
+        </Head>
+        <Header />
+          {props.children}
+        <Footer />
+      </div>
     </main>
   );
 }
