f4b941033510c852e8ea25658cd7350d39938fdd
your username <muzzammilz@gmail.com>
2021-09-18 23:25:51 +0500

    Added about picture

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import DefaultLayout from '@layouts/default';
-import Heading from '@components/heading'; 
+import Heading from '@components/heading';
+
+import profilePicture from '../images/profile_picture.webp';
 
 import { getAllPosts } from '@api';
 
@@ -9,6 +12,13 @@ export default function Blog(props) {
   return (
     <DefaultLayout title={props.title} description={props.description}>
       <Heading text={props.title} />
+      <div className="flex flex-col mx-auto w-64 h-64 mt-5">
+        <Image 
+          src={profilePicture}
+          alt='Profile picture of Muzzammil Hussain'
+          className='object-cover mr-2 rounded-full'
+        />
+      </div>
     </DefaultLayout>
   );
 }
