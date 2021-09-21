88f70b18004e2a1ad8017768ea387f44cb9853d7
your username <muzzammilz@gmail.com>
2021-09-21 15:23:54 +0500

    Improved spacing

--- components/hero.js
+++ components/hero.js
@@ -6,20 +6,22 @@ import heroImage from '../images/undraw_Coding_re_iv62.png';
 export default function Hero() {
   return (
     <>
-      <div className="max-w-xl mx-auto">
-        <Image src={heroImage} alt="Muzzammil Hussain" />
-      </div>
-      <div className="text-center text-1xl sm:text-2xl">
-        Hello, my name is 
-      </div>
-      <div className="text-center font-bold text-4xl sm:text-6xl">
-        Muzzammil Hussain
-      </div>
-      <div className="text-center text-1xl  xl sm:text-2xl">
-        I make software that helps businesses grow
-      </div>
-      <div className="text-center mt-3">
-        <Btn text="Learn More" link="/about" />
+      <div className="mb-20">
+        <div className="max-w-xl mx-auto">
+          <Image src={heroImage} alt="Muzzammil Hussain" />
+        </div>
+        <div className="text-center text-1xl sm:text-2xl">
+          Hello, my name is 
+        </div>
+        <div className="text-center font-bold text-4xl sm:text-6xl">
+          Muzzammil Hussain
+        </div>
+        <div className="text-center text-1xl  xl sm:text-2xl">
+          I make software that helps businesses grow
+        </div>
+        <div className="text-center mt-3">
+          <Btn text="Learn More" link="/about" />
+        </div>
       </div>
     </>
   );
