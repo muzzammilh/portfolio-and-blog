48220f3ff6021d6aa4739f02b68bbc58797ce904
your username <muzzammilz@gmail.com>
2021-09-21 15:45:02 +0500

    Added basic publication component formatting

--- components/publication.js
+++ components/publication.js
@@ -1,11 +1,30 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Publication(post) {
+
+  const prettyDate = new Date(post.dateAdded).toLocaleString('en-US', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  });
+
   return (
     <>
-      <h1>Publication</h1>
-      <p>{post.title}</p>
-      <p>{post.dateAdded}</p>
+      <div className="mt-10 mb-10 text-center">
+        <div className="">{prettyDate}</div>
+        <div className="text-xl sm:text-2xl mt-2 mb-2">{post.title}</div>
+        <div className="text-center mt-5 mb-5">
+          <Image
+            src={post.coverImage}
+            alt={post.title}
+            height={300}
+            width={600}
+          />
+        </div>
+        <div>{post.brief}</div>
+        <div className="mt-10 border-b-2 border-gray-200"></div>
+      </div>
     </>
   );
 }
